c5a581a444dfd3c005e09da02fbb9eaae332f48d
Ian Holladay <holladayian@gmail.com>
2021-05-22 15:47:59 -0600

    Import Header.css

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Header.css';
 import wing from '../assets/wing.png';
 
 export const Header = () => {
     return (
-      <div className="Header">
+      <div className="header">
         <img src={wing} alt="Wing" className="wing" />
-        Header
+        Hermes Shuffle
         <img src={wing} alt="Wing" className="wing wingRight" />
       </div>
     );
