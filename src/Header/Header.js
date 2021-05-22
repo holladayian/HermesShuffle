419c5781b1e82801651acf411619d910cb89a784
Ian Holladay <holladayian@gmail.com>
2021-05-22 15:21:09 -0600

    Import wing picture into header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import wing from '../assets/wing.png';
 
 export const Header = () => {
     return (
       <div className="Header">
+        <img src={wing} alt="Wing" className="wing" />
         Header
+        <img src={wing} alt="Wing" className="wing wingRight" />
       </div>
     );
 }
