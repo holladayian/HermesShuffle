aa5eb6ea8c4aa99cb085268d08695c2d5717380a
Ian Holladay <holladayian@gmail.com>
2021-05-23 16:24:37 -0600

    Add CardShuffler object with wash function

---
+++ src/CardShuffler/CardShuffler.js
@@ -0,0 +1,15 @@
+export const CardShuffler = {
+
+  wash(deck) {
+      let i = deck.length,
+        j = 0,
+        temp;
+      while (i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+      }
+      return deck;
+  }
+}
