9d4d8c6d72363b861c35b3c6544ef353e00b9f8f
Ian Holladay <holladayian@gmail.com>
2021-05-23 14:50:15 -0600

    Add class=card to images, and filler description

--- src/Shuffler/Shuffler.js
+++ src/Shuffler/Shuffler.js
@@ -7,7 +7,7 @@ export class Shuffler extends Component {
     super()
     this.state = {
       cardList: ImageLoader,
-      shuffleFunction: () => null,
+      shuffleFunction: () => "null",
       description: "",
       cardOrder: [],
     }
@@ -22,12 +22,16 @@ export class Shuffler extends Component {
           <button className="riffle s-button">Riffle</button>
           <button className="organize s-button">Organize</button>
         </div>
-        <div className="shuffle-description">
-
-        </div>
-        <button className="shuffle s-button">Shuffle</button>
+        {this.state.shuffleFunction() && 
+          <>
+            <div className="shuffle-description">
+              {this.state.description || "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+            </div>
+            <button className="shuffle s-button">Shuffle</button>
+          </>
+        }
         <div className="card-list">
-          {this.state.cardList.map(card => <img src={card} alt={card}/>)}
+          {this.state.cardList.map(card => <img src={card} alt={card} class="card"/>)}
         </div>
       </div>
     );
