ba80944fc42e3c8aea0edb12c22e7ef5d96255ee
Ian Holladay <holladayian@gmail.com>
2021-05-22 18:30:48 -0600

    Add links to buttons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 import wing from '../assets/wing.png';
 
 export const Header = () => {
@@ -11,9 +12,15 @@ export const Header = () => {
           <img src={wing} alt="Wing" className="wing wingRight" />
         </div>
         <div className="header-bottom">
-          <button className="home-button">Home</button>
-          <button className="shuffler-button">Shuffler</button>
-          <button className="about-button">About</button>
+          <Link to='/'>
+            <button className="home-button">Home</button>
+          </Link>
+          <Link to='/shuffler'>
+            <button className="shuffler-button">Shuffler</button>
+          </Link>
+          <Link to='/about'>
+            <button className="about-button">About</button>
+          </Link>
         </div>
       </section>
     );
