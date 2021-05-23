c1a8d9f4936e5ae336b65649846f146c3f2479fc
Ian Holladay <holladayian@gmail.com>
2021-05-23 16:25:11 -0600

    Implement wash functionality

--- src/Shuffler/Shuffler.js
+++ src/Shuffler/Shuffler.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ImageLoader } from '../ImageLoader/ImageLoader.js';
+import { CardShuffler } from '../CardShuffler/CardShuffler.js';
 import './Shuffler.css';
 
 export class Shuffler extends Component {
@@ -7,17 +8,21 @@ export class Shuffler extends Component {
     super()
     this.state = {
       cardList: ImageLoader,
-      shuffleFunction: () => "null",
+      shuffleFunction: () => null,
       description: "",
       cardOrder: [],
     }
+  }
+
+  shuffle = (newOrder) => {
+    this.setState({ cardList: newOrder })
   }
 
   render() {
     return (
       <div className="shuffler" data-testid="shuffler">
         <div className="button-section" data-testid="button-section">
-          <button className="wash s-button" data-testid="wash">Wash</button>
+          <button className="wash s-button" onClick={() => this.shuffle(CardShuffler.wash(this.state.cardList))} data-testid="wash">Wash</button>
           <button className="overhand s-button" data-testid="overhand">Overhand</button>
           <button className="riffle s-button" data-testid="riffle">Riffle</button>
           <button className="organize s-button" data-testid="organize">Organize</button>
@@ -27,7 +32,7 @@ export class Shuffler extends Component {
             <div className="shuffle-description" data-testid="shuffle-description">
               {this.state.description || "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
             </div>
-            <button className="shuffle s-button" data-testid="shuffle">Shuffle</button>
+            <button className="shuffle s-button" data-testid="shuffle" onClick={() => this.shuffle()}>Shuffle</button>
           </>
         }
         <div className="card-list" data-testid="card-list">
