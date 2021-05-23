3176e7f867b39377c12d03b28a8cb1d9ccc6373f
Ian Holladay <holladayian@gmail.com>
2021-05-23 13:17:32 -0600

    Add buttons and import ImageLoader

--- src/Shuffler/Shuffler.js
+++ src/Shuffler/Shuffler.js
@@ -1,47 +1,36 @@
 import React, { Component } from 'react';
+import { ImageLoader } from '../ImageLoader/ImageLoader.js';
 import './Shuffler.css';
 
 export class Shuffler extends Component {
   constructor() {
     super()
     this.state = {
-      cardList: [
-        "AS", "AH", "AD", "AC", 
-        "KS", "KH", "KD", "KC", 
-        "QS", "QH", "QD", "QC", 
-        "JS", "JH", "JD", "JC", 
-        "10S", "10H", "10D", "10C", 
-        "9S", "9H", "9D", "9C", 
-        "8S", "8H", "8D", "8C", 
-        "7S", "7H", "7D", "7C", 
-        "6S", "6H", "6D", "6C", 
-        "5S", "5H", "5D", "5C", 
-        "4S", "4H", "4D", "4C", 
-        "3S", "3H", "3D", "3C", 
-        "2S", "2H", "2D", "2C", 
-      ],
+      cardList: ImageLoader,
       shuffleFunction: () => null,
       description: "",
       cardOrder: [],
     }
   }
+
   render() {
     return (
       <div className="shuffler">
         <div className="button-section">
-          <button className="wash-button">Wash</button>
-          <button className="overhand-button">Overhand</button>
-          <button className="riffle-button">Riffle</button>
-          <button className="organize-button">Organize</button>
+          <button className="wash s-button">Wash</button>
+          <button className="overhand s-button">Overhand</button>
+          <button className="riffle s-button">Riffle</button>
+          <button className="organize s-button">Organize</button>
         </div>
         <div className="shuffle-description">
 
         </div>
-        <button className="shuffle-button">Shuffle</button>
+        <button className="shuffle s-button">Shuffle</button>
         <div className="card-list">
-
+          {this.state.cardList.map(card => <img src={card} alt={card}/>)}
         </div>
       </div>
     );
   }
 }
+
