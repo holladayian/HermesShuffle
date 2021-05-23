34ca7d6a2ac98b39c3f44f3bc4b6d49003e26670
Ian Holladay <holladayian@gmail.com>
2021-05-22 18:19:01 -0600

    Add navigation buttons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,10 +4,17 @@ import wing from '../assets/wing.png';
 
 export const Header = () => {
     return (
-      <div className="header">
-        <img src={wing} alt="Wing" className="wing" />
-        Hermes Shuffle
-        <img src={wing} alt="Wing" className="wing wingRight" />
-      </div>
+      <section className="header">
+        <div className="header-top">
+          <img src={wing} alt="Wing" className="wing" />
+          Hermes Shuffle
+          <img src={wing} alt="Wing" className="wing wingRight" />
+        </div>
+        <div className="header-bottom">
+          <button className="home-button">Home</button>
+          <button className="shuffler-button">Shuffler</button>
+          <button className="about-button">About</button>
+        </div>
+      </section>
     );
 }
