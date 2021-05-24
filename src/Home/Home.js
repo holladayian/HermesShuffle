ccff0427aab4c3c231b27828b970c55efc594876
Ian Holladay <holladayian@gmail.com>
2021-05-23 21:44:53 -0600

    Add Home.js Home.css and Home.test.js

---
+++ src/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.css';
+
+export const Home = () => {
+    return (
+      <section className="home" data-testid="home">
+        
+      </section>
+    );
+}
