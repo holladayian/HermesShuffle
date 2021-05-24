9d101a24fb644ba1d46d80923ca4b5cdd3281209
Ian Holladay <holladayian@gmail.com>
2021-05-23 19:25:06 -0600

    Comment out overhand and riffle for time constraints

--- src/CardShuffler/CardShuffler.js
+++ src/CardShuffler/CardShuffler.js
@@ -17,5 +17,33 @@ export const CardShuffler = {
     return deck.sort((a, b) => {
       return a.index - b.index
     })
-  }
+  },
+
+  fisherYates(deck) {
+    let count = deck.length;
+    while (count) {
+      deck.push(deck.splice(Math.floor(Math.random() * count), 1)[0]);
+      count -= 1;
+    }
+    return deck
+  },
+
+
+  // In progress
+
+  // overhand(deck) {
+  //   let count = deck.length;
+  //   // let newOrder = [];
+  //   // for (let i = (count -= 10); i > 10; i -= 10) {
+  //   // for (let i = 0; i < 5; i += 1) {
+  //   for (let i = 0; i < (count -= count%10); i += 10) {
+  //     console.log(deck)
+  //     // deck.push(deck.splice(0, i))
+  //     let splicedSection = deck.splice(41, 10)
+  //     deck.unshift(splicedSection)
+  //   }
+  //   // deck.push(newOrder)
+  //   console.log(deck.flat())
+  //   return deck.flat()
+  // }
 }
