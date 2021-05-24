577395e366e37318401b2ab0c5d61cb63d09271f
Ian Holladay <holladayian@gmail.com>
2021-05-23 22:24:36 -0600

    Add data-testids to Home elements

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,8 +4,8 @@ import './Home.css';
 export const Home = () => {
     return (
       <section className="home" data-testid="home">
-        <h1>Welcome to Hermes Shuffle!</h1>
-        <p>
+        <h1 data-testid="h1">Welcome to Hermes Shuffle!</h1>
+        <p data-testid="p">
           This was a take home Engineering Challenge from Altvia.
           <br/>
           The goal? Design and implement a card shuffler using the language of your choice.
