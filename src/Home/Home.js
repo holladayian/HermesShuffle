20143acd89c9bd25c40fb44cfe802c4c5b6a016c
Ian Holladay <holladayian@gmail.com>
2021-05-23 22:06:17 -0600

    Add text explaining what this project is about

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,6 +4,17 @@ import './Home.css';
 export const Home = () => {
     return (
       <section className="home" data-testid="home">
+        <h1>Welcome to Hermes Shuffle!</h1>
+        <p>
+          This was a take home Engineering Challenge from Altvia.
+          <br/>
+          The goal? Simple: Design and implement a card shuffler using the language of your choice.
+          <br/>
+          <br/>
+          Check out it's functionality in the Shuffler page! 
+          <br/>
+          Or check out the repository <a href="https://github.com/holladayian/HermesShuffle" target="_blank">here!</a>
+        </p>
         
       </section>
     );
